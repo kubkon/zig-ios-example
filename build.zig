699f0d59b6b024321788115e6fa28e0a94bc7c09
Jakub Konka <kubkon@jakubkonka.com>
2021-12-22 11:27:23 +0100

    Update build script to zig 0.9.0 changes

--- build.zig
+++ build.zig
@@ -4,7 +4,19 @@ const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) !void {
     const mode = b.standardReleaseOptions();
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{
+        .whitelist = &[_]std.zig.CrossTarget{
+            .{ .cpu_arch = .aarch64, .os_tag = .ios },
+            .{ .cpu_arch = .aarch64, .os_tag = .ios, .abi = .simulator },
+            .{ .cpu_arch = .x86_64, .os_tag = .ios, .abi = .simulator },
+        },
+    });
+
+    if (b.sysroot == null) {
+        std.log.warn("You haven't set the path to Apple SDK which may lead to build errors.", .{});
+        std.log.warn("Hint: you can the path to Apple SDK with --sysroot <path> flag like so:", .{});
+        std.log.warn("  zig build --sysroot $(xcrun --sdk iphoneos --show-sdk-path) -Dtarget=aarch64-ios", .{});
+    }
 
     const exe = b.addExecutable("app", "main.zig");
     b.default_step.dependOn(&exe.step);
@@ -15,12 +27,9 @@ pub fn build(b: *Builder) !void {
     exe.linkLibC();
     exe.linkFramework("Foundation");
     exe.linkFramework("UIKit");
-
-    if (!std.Target.current.isDarwin()) {
-        exe.addSystemIncludeDir("/usr/include");
-        exe.addLibPath("/usr/lib");
-        exe.addFrameworkDir("/System/Library/Frameworks");
-    }
+    exe.addSystemIncludeDir("/usr/include");
+    exe.addLibPath("/usr/lib");
+    exe.addFrameworkDir("/System/Library/Frameworks");
 
     exe.install();
 
